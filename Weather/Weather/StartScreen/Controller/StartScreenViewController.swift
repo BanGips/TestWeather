d12970ce20ef3148a47eeec9d407ddc67b0dcbe0
BanGips <bangips2@gmail.com>
2020-09-21 15:25:01 +0300

    add project

---
+++ Weather/Weather/StartScreen/Controller/StartScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by BanGips on 9/21/20.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
