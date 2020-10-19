987de898bc82cc844390fc6150c5b6f5361a7d82
BanGips <bangips2@gmail.com>
2020-10-19 17:24:10 +0300

    Fix initial VC through code

---
+++ Weather/Weather/Application/LocationManager/LocationManager.swift
@@ -0,0 +1,33 @@
+//
+//  LocationManager .swift
+//  Weather
+//
+//  Created by BanGips on 10/19/20.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationManager()
+    
+    private var locationManager: CLLocationManager?
+    var currentLocation: CLLocationCoordinate2D?
+    
+    func setupLocation() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first?.coordinate
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) { }
+}
