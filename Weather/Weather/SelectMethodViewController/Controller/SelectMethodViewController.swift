cd09a6d48b1a4c0f9d5da3680a6ecafe228a4ad8
BanGips <bangips@MacBook-Pro-BanGips.local>
2020-10-26 15:55:23 +0300

    fix location manager

--- Weather/Weather/SelectMethodViewController/Controller/SelectMethodViewController.swift
+++ Weather/Weather/SelectMethodViewController/Controller/SelectMethodViewController.swift
@@ -23,8 +23,8 @@ class SelectMethodViewController: UIViewController {
 
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         LocationManager.shared.setupLocation()
     }
